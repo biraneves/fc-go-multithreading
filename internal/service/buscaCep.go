60f67c1946dfb70f1ce574ab59564069b6d8531f
Bira Neves <biraneves@outlook.com>
2025-02-18 14:12:54 -0300

    feat: :sparkles: add function urlServico
    Given a service and a zip code, this function returns the url to be used.

---
+++ internal/service/buscaCep.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/biraneves/fc-go-multithreading/internal/model"
+)
+
+func BuscaCep(servico int, cep string) (*model.ViaCepApi, *model.CepBrasilApi, error) {
+	return nil, nil, nil
+}
+
+func urlServico(servico int, cep string) (string, error) {
+	var url string
+
+	switch servico {
+	case model.CepBrasil:
+		url = "https://brasilapi.com.br/api/cep/v1/{cep}"
+	case model.ViaCep:
+		url = "https://viacep.com.br/ws/{cep}/json/"
+	default:
+		url = ""
+	}
+
+	if url == "" {
+		return "", fmt.Errorf("serviço desconhecido")
+	}
+
+	finalUrl := strings.ReplaceAll(url, "{cep}", cep)
+
+	return finalUrl, nil
+}
